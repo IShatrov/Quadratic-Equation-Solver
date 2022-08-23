349d0d2b8458119111c0931e3a49fdfa9d7aa614
IShatrov <shatrov2004@mail.ru>
2022-08-23 17:39:39 +0300

    Check for NAN and INF

--- quadratic_equation_solver/main.cpp
+++ quadratic_equation_solver/main.cpp
@@ -6,8 +6,8 @@
 #define MAX_OF_TWO(n1, n2) ((n1 > n2) ? (n1) : (n2))
 #define MIN_OF_TWO(n1, n2) ((n1 > n2) ? (n2) : (n1))
 #define RULES "Use point as the decimal separator. Numbers must be shorter than 1000 symbols.\n"
-#define EPSILON 0e-7
-#define INF 2e6
+#define EPSILON 1e-7
+#define INF_ROOTS 3
 
 void getnum(double * num);
 int solve(double a, double b, double c, double * x1, double * x2);
@@ -24,9 +24,9 @@ int main(void)
     getnum(&b);
     printf("Please enter c.\n");
     getnum(&c);
-    printf("Solving %.4fx^2 + %.4fx + %.4f = 0\n", a, b, c);
     double x1 = NAN, x2 = NAN;
     int roots_amount = solve(a, b, c, &x1, &x2);
+    printf("Solving %.4fx^2 + %.4fx + %.4f = 0\n", a, b, c);
     switch (roots_amount)
     {
         case 2:
@@ -38,7 +38,7 @@ int main(void)
         case 0:
             printf("No real solutions.");
             break;
-        case INF:
+        case INF_ROOTS:
             printf("x can be any real number.");
             break;
         default:
@@ -50,7 +50,7 @@ int main(void)
 
 void getnum(double * num)
 {
-    assert(num);
+    assert(num != NULL);
     double n = NAN;
     scanf("%lf", &n);
     int c = getchar();
@@ -69,8 +69,15 @@ void getnum(double * num)
 
 int solve(double a, double b, double c, double * x1, double * x2)
 {
-    assert(x1);
-    assert(x2);
+    assert(!isnan(a));
+    assert(!isnan(b));
+    assert(!isnan(c));
+    assert(!isinf(a));
+    assert(!isinf(b));
+    assert(!isinf(c));
+    assert(x1 != NULL);
+    assert(x2 != NULL);
+    assert(x1 != x2);
     double d = b * b - 4 * a * c;
     if (!are_doubles_equal(a, 0))
     {
@@ -93,7 +100,7 @@ int solve(double a, double b, double c, double * x1, double * x2)
     }
     else
     {
-        if (are_doubles_equal(b, 0)) return ((are_doubles_equal(c, 0)) ? INF : 0);
+        if (are_doubles_equal(b, 0)) return ((are_doubles_equal(c, 0)) ? INF_ROOTS : 0);
         else
         {
             *x1 = -c / b;
@@ -105,5 +112,9 @@ int solve(double a, double b, double c, double * x1, double * x2)
 
 int are_doubles_equal(double n1, double n2)
 {
-    return ((fabs(n1 - n2) <= EPSILON) ? (1) : (0));
+    assert(!isnan(n1));
+    assert(!isnan(n2));
+    assert(!isinf(n1));
+    assert(!isinf(n2));
+    return (fabs(n1 - n2) <= EPSILON);
 }
