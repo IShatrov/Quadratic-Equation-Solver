00dfb833a17499afc14732f29d9d33bfb93616a5
IShatrov <shatrov2004@mail.ru>
2022-08-24 10:23:35 +0300

    Empty lines
    Relates to #11

--- quadratic_equation_solver/main.cpp
+++ quadratic_equation_solver/main.cpp
@@ -18,6 +18,7 @@ void clear_buffer(void);
 int main(void)
 {
     double a = NAN, b = NAN, c = NAN;
+
     printf("ax^2 + bx + c = 0\n");
     printf(RULES);
     printf("Please enter a.\n");
@@ -26,8 +27,10 @@ int main(void)
     getnum(&b);
     printf("Please enter c.\n");
     getnum(&c);
+
     double x1 = NAN, x2 = NAN;
     int roots_amount = solve_quadr(a, b, c, &x1, &x2);
+
     printf("Solving %.4fx^2 + %.4fx + %.4f = 0\n", a, b, c);
     switch (roots_amount)
     {
@@ -53,9 +56,11 @@ int main(void)
 void getnum(double *num)
 {
     assert(num != NULL);
+
     double n = NAN;
     scanf("%lf", &n);
     int c = getchar();
+
     while (c != '\n')
     {
         clear_buffer();
@@ -77,6 +82,7 @@ int solve_quadr(double a, double b, double c, double *x1, double *x2)
     assert(x1 != NULL);
     assert(x2 != NULL);
     assert(x1 != x2);
+
     double d = b*b - 4*a*c;
     if (!are_doubles_equal(a, 0))
     {
@@ -111,6 +117,7 @@ int solve_lin(double a, double b, double *x)
     assert(!isinf(a));
     assert(!isinf(b));
     assert(x != NULL);
+
     if (are_doubles_equal(a, 0)) return ((are_doubles_equal(b, 0)) ? INF_ROOTS : 0);
     else
     {
@@ -125,6 +132,7 @@ int are_doubles_equal(double n1, double n2)
     assert(!isnan(n2));
     assert(!isinf(n1));
     assert(!isinf(n2));
+
     return (fabs(n1 - n2) <= EPSILON);
 }
 
